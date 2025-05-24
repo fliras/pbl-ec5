987be12a2cc936aacd65ea4f702adc88abcfb418
Felipe Lira <filipi6219@gmail.com>
2025-05-24 09:50:28 -0300

    refactor: limpa algumas controllers

--- code/web/Weathuino/Weathuino/Weathuino/Controllers/DashboardsController.cs
+++ code/web/Weathuino/Weathuino/Weathuino/Controllers/DashboardsController.cs
@@ -1,11 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Diagnostics;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Mvc;
-using Microsoft.Extensions.Logging;
-using Weathuino.Models;
+﻿using Microsoft.AspNetCore.Mvc;
 
 namespace Weathuino.Controllers
 {
