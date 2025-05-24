592cefeb7c4928105d1692d2332e3cbebf2b5e8b
Felipe Lira <filipi6219@gmail.com>
2025-05-24 16:00:21 -0300

    fix: corrige controller

--- code/web/Weathuino/Weathuino/Weathuino/Controllers/HomeController.cs
+++ code/web/Weathuino/Weathuino/Weathuino/Controllers/HomeController.cs
@@ -1,6 +1,12 @@
-﻿namespace Weathuino.Controllers
+﻿using Weathuino.Enums;
+
+namespace Weathuino.Controllers
 {
     public class HomeController : PadraoController
     {
+        public HomeController()
+        {
+            AcessoExigido = PerfisAcesso.COMUM;
+        }
     }
 }
