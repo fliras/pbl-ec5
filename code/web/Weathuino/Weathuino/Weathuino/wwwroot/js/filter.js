a7d70c6ccb4048aec810ec45340f2b465d64267d
Felipe Lira <filipi6219@gmail.com>
2025-06-01 16:59:30 -0300

    feat: implementa novo layout no crud de medidores

--- code/web/Weathuino/Weathuino/Weathuino/wwwroot/js/filter.js
+++
@@ -1,9 +0,0 @@
-﻿function applyFilter() {
-    // logica do filtro
-    alert('Filtrado');
-}
-function deletaMedidor(id) {
-    const deveDeletar = confirm('Tem certeza que deseja excluir este medidor?');
-    if (deveDeletar)
-        document.location.href = `/Medidores/Delete?id=${id}`;
-}
---
+++ code/web/Weathuino/Weathuino/Weathuino/wwwroot/js/medidores.js
@@ -0,0 +1,24 @@
+﻿function deletaMedidor(id) {
+	const deveDeletar = confirm('Tem certeza que deseja excluir este medidor?');
+	if (deveDeletar)
+		document.location.href = `/Medidores/Delete?id=${id}`;
+}
+
+function filtraMedidores() {
+	const id = document.getElementById('id').value;
+	const nome = document.getElementById('nome').value;
+	$.ajax({
+		url: "/medidores/ConsultaComFiltros",
+		data: { id, nome },
+		success: function (dados) {
+			if (dados.erro) {
+				alert(dados.msg);
+			}
+			else {
+				document.getElementById('dadosMedidores').innerHTML = dados;
+			}
+		},
+	});
+}
+
+window.onload = filtraMedidores;
