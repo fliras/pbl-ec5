0a452ed58ec5f0bc45d0f3f1b75d5334c0a61e66
Felipe Lira <filipi6219@gmail.com>
2025-06-01 17:19:42 -0300

    feat: implementa novo layout no crud de usuários

---
+++ code/web/Weathuino/Weathuino/Weathuino/wwwroot/js/usuarios.js
@@ -0,0 +1,26 @@
+﻿function deletaRegistro(id) {
+	const deveDeletar = confirm('Tem certeza que deseja excluir este registro?');
+	if (deveDeletar)
+		document.location.href = `/Usuarios/Delete?id=${id}`;
+}
+
+function filtraUsuarios() {
+	const id = document.getElementById('id').value;
+	const nome = document.getElementById('nome').value;
+	const email = document.getElementById('email').value;
+
+	$.ajax({
+		url: "/usuarios/ConsultaComFiltros",
+		data: { id, nome, email },
+		success: function (dados) {
+			if (dados.erro) {
+				alert(dados.msg);
+			}
+			else {
+				document.getElementById('dadosUsuarios').innerHTML = dados;
+			}
+		},
+	});
+}
+
+window.onload = filtraUsuarios;
