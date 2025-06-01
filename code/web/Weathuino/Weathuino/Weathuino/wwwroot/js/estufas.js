2b40744432d8a8060a2321c9e34f7b38cbdab8ed
Felipe Lira <filipi6219@gmail.com>
2025-06-01 17:53:50 -0300

    feat: implementa novo layout no crud de estufas

---
+++ code/web/Weathuino/Weathuino/Weathuino/wwwroot/js/estufas.js
@@ -0,0 +1,25 @@
+﻿function deletaRegistro(id) {
+	const deveDeletar = confirm('Tem certeza que deseja excluir este registro?');
+	if (deveDeletar)
+		document.location.href = `/Estufas/Delete?id=${id}`;
+}
+
+function filtraEstufas() {
+	const id = document.getElementById('id').value;
+	const nome = document.getElementById('nome').value;
+
+	$.ajax({
+		url: "/estufas/ConsultaComFiltros",
+		data: { id, nome },
+		success: function (dados) {
+			if (dados.erro) {
+				alert(dados.msg);
+			}
+			else {
+				document.getElementById('dadosEstufas').innerHTML = dados;
+			}
+		},
+	});
+}
+
+window.onload = filtraEstufas;
