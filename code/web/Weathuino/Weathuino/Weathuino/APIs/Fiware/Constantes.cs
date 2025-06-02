9bc89b46fa54b84921fcbbbeae654ff05227a897
Felipe Lira <filipi6219@gmail.com>
2025-06-02 15:39:45 -0300

    refactor: remove urls mockadas

--- code/web/Weathuino/Weathuino/Weathuino/APIs/Fiware/Constantes.cs
+++ code/web/Weathuino/Weathuino/Weathuino/APIs/Fiware/Constantes.cs
@@ -5,17 +5,11 @@ namespace Weathuino.APIs.Fiware
     public class Constantes
     {
         public static readonly string IP_SERVIDOR = "52.1.120.205";
+
         public static readonly string URL_NOVO_IOT = $"http://{IP_SERVIDOR}:4041/iot/devices";
-        //public static readonly string URL_NOVO_IOT = $"https://testefiware.free.beeceptor.com";
-
         public static readonly string URL_REGISTRO_ATRIBUTOS = $"http://{IP_SERVIDOR}:1026/v2/subscriptions";
-        //public static readonly string URL_REGISTRO_ATRIBUTOS = $"https://testefiware.free.beeceptor.com";
-
         public static readonly string URL_EXCLUSAO_DISPOSITIVO_AGENT_MQTT = $"http://{IP_SERVIDOR}:4041/iot/devices";
-        //public static readonly string URL_EXCLUSAO_DISPOSITIVO_AGENT_MQTT = $"https://testefiware.free.beeceptor.com";
-
         public static readonly string URL_EXCLUSAO_DISPOSITIVO_ORION = $"http://{IP_SERVIDOR}:1026/v2/entities";
-        //public static readonly string URL_EXCLUSAO_DISPOSITIVO_ORION = $"https://testefiware.free.beeceptor.com";
 
         public static string ENTITY_NAME(int entityNameID)
         {
