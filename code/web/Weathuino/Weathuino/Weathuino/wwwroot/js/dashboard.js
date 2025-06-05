3842e25a3062f0bd26884b13ca3ad68e924e91cf
Felipe Lira <filipi6219@gmail.com>
2025-06-05 01:03:13 -0300

    refactor: abstrai funções de dashboard + comentários

--- code/web/Weathuino/Weathuino/Weathuino/wwwroot/js/dashboard.js
+++ code/web/Weathuino/Weathuino/Weathuino/wwwroot/js/dashboard.js
@@ -1,24 +1,115 @@
-document.addEventListener("DOMContentLoaded", function() {
-    loadDashboard("#dashboard1", "/Dashboards/GetDashboard1");
-    loadDashboard("#dashboard2", "/Dashboards/GetDashboard2");
-});
+function montaInstanciaDeGrafico(ctx, objPontosAcumulados, objAnnotations) {
+	return new Chart(ctx, {
+		type: 'line',
+		data: {
+			datasets: [
+				{
+					label: 'Temperatura (\u00B0C)',
+					data: objPontosAcumulados,
+					borderColor: 'rgba(75, 192, 192, 1)',
+					backgroundColor: 'rgba(75, 192, 192, 0.2)',
+					tension: 0.1
+				},
+			]
+		},
+		options: {
+			scales: {
+				x: {
+					type: 'time',
+					time: {
+						tooltipFormat: 'dd/MM/yyyy HH:mm:ss',
+						displayFormats: {
+							second: 'dd/MM/yyyy HH:mm:ss',
+							minute: 'dd/MM/yyyy HH:mm:ss',
+							hour: 'dd/MM/yyyy HH:mm:ss'
+						}
+					},
+					title: {
+						display: true,
+						text: 'Data e Hora'
+					}
+				},
+				y: {
+					title: {
+						display: true,
+						text: 'Temperatura (\u00B0C)'
+					}
+				}
+			},
+			plugins: {
+				annotation: {
+					annotations: objAnnotations
+				},
+				legend: {
+					display: true
+				}
+			},
+		}
+	});
+}
 
-function loadDashboard(containerSelector, url) {
-    const container = document.querySelector(containerSelector);
-    if (!container) return;
-    
-    fetch(url)
-    .then(response => {
-      if (!response.ok) {
-        throw new Error("Network error");
-      }
-    return response.text();
-    })
-    .then(html => {
-        container.innerHTML = html;
-    })
-    .catch(err => {
-        console.error(err);
-    container.innerHTML = "<p class='fallback-text text-danger'>Erro ao carregar o dashboard.</p>";
-    });
+// plota duas linhas fixas no gráfico que representam as temperaturas mínimas e máximas da estufa
+function montaRangesTemperatura(objAnnotations, tempMinima, tempMaxima) {
+	objAnnotations.tempMinima = {
+		type: 'line',
+		yMin: tempMinima,
+		yMax: tempMinima, // linha horizontal no Y = 30
+		borderColor: 'red',
+		borderWidth: 2,
+		label: {
+			display: true,
+			content: `Temperatura Min.: ${tempMinima}\u00B0C`,
+			position: 'end',
+			backgroundColor: 'rgba(255,0,0,0.2)',
+			color: 'red',
+			yAdjust: -10
+		}
+	}
+	objAnnotations.tempMaxima = {
+		type: 'line',
+		yMin: tempMaxima,
+		yMax: tempMaxima, // linha horizontal no Y = 30
+		borderColor: 'red',
+		borderWidth: 2,
+		label: {
+			display: true,
+			content: `Temperatura Max.: ${tempMaxima}\u00B0C`,
+			position: 'end',
+			backgroundColor: 'rgba(255,0,0,0.2)',
+			color: 'red',
+		}
+	}
+}
+
+function montaAnnotationDoSetPoint(valorSetPoint) {
+	return {
+		type: 'line',
+		yMin: valorSetPoint,
+		yMax: valorSetPoint, // linha horizontal no Y = 30
+		borderColor: 'red',
+		borderWidth: 2,
+		label: {
+			display: true,
+			content: `SP.: ${valorSetPoint}\u00B0C`,
+			position: 'end',
+			backgroundColor: 'rgba(255,0,0,0.2)',
+			color: 'red',
+			yAdjust: -10
+		}
+	}
+}
+
+function formataTimestamp(datahora) {
+	return datahora.toLocaleString('pt-BR', {
+		day: '2-digit',
+		month: '2-digit',
+		year: 'numeric',
+		hour: '2-digit',
+		minute: '2-digit',
+		second: '2-digit'
+	}).replace(',', '');
+}
+
+window.onload = () => {
+	montaChart();
 }
